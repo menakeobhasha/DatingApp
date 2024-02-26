c60959010b1bd697205ddc049be76734f5c9afa6
Menake <menakeobhasha1@gmail.com>
2024-02-26 20:37:29 +0530

    End of section 8

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace API;
+
+public static class DateTimeExtensions
+{
+    public static int CalculateAge(this DateOnly dbo)
+    {
+        var today = DateOnly.FromDateTime(DateTime.UtcNow);
+
+        var age = today.Year - dbo.Year;
+
+        if (dbo > today.AddYears(-age)) age--;
+
+        return age;
+    }
+}
